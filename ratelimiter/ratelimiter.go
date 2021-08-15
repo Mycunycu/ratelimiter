933118d338054e5044d331e824a07df0afe9ac3c
Mycunycu <37483790+Mycunycu@users.noreply.github.com>
2021-08-15 18:30:09 +0300

    Add files via upload

---
+++ ratelimiter/ratelimiter.go
@@ -0,0 +1,103 @@
+package ratelimiter
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// RateLimiter object
+type RateLimiter struct {
+	chanIn        chan func()
+	rateChan      chan struct{}
+	maxTask       uint32
+	limit         uint32
+	limitInterval time.Duration
+	count         uint32
+	wg            *sync.WaitGroup
+}
+
+// New - RateLimiter constructor
+// taskChan - channel for getting a tasks
+// max - maximum allowed count parallel tasks
+// limit - rate limit in a time window
+// interval - time window with limit
+func New(taskChan chan func(), max, limit uint32, interval time.Duration) *RateLimiter {
+	if max == 0 {
+		max = 1
+	}
+
+	if limit == 0 {
+		limit = 50
+	}
+
+	return &RateLimiter{
+		chanIn:        taskChan,
+		rateChan:      make(chan struct{}, limit),
+		maxTask:       max,
+		limit:         limit,
+		limitInterval: interval,
+		wg:            &sync.WaitGroup{},
+	}
+}
+
+// DoWork - run to working the instance RateLimiter
+func (rl *RateLimiter) DoWork() {
+	rl.initLimit()
+
+	for i := 0; i < int(rl.maxTask); i++ {
+		rl.wg.Add(1)
+		go rl.taskWorker()
+	}
+
+	go rl.renewLimitByInterval()
+}
+
+func (rl *RateLimiter) initLimit() {
+	for i := 0; i < int(rl.limit); i++ {
+		rl.rateChan <- struct{}{}
+	}
+}
+
+func (rl *RateLimiter) taskWorker() {
+	for {
+		select {
+		case task, ok := <-rl.chanIn:
+			if !ok {
+				fmt.Println("ChanIn closed")
+				rl.wg.Done()
+				return
+			}
+
+			<-rl.rateChan
+			atomic.AddUint32(&rl.count, 1)
+			task()
+		}
+	}
+
+}
+
+func (rl *RateLimiter) renewLimitByInterval() {
+	ticker := time.NewTicker(rl.limitInterval)
+
+	for range ticker.C {
+		rl.renewLimit()
+	}
+}
+
+func (rl *RateLimiter) renewLimit() {
+	needToAdd := atomic.LoadUint32(&rl.count)
+	atomic.StoreUint32(&rl.count, 0)
+
+	for i := 0; i < int(needToAdd); i++ {
+		rl.rateChan <- struct{}{}
+	}
+}
+
+// Wait - waiting while all workers which process tasks in work
+// The signal to stop working - close the task chanel
+// IMPORTANT - close the channel with caution to avoid panic, don't write into the closed channel.
+func (rl *RateLimiter) Wait() {
+	rl.wg.Wait()
+}
---
+++ ratelimiter_example.go
@@ -0,0 +1,39 @@
+package ratelimiter_example
+
+import (
+	"fmt"
+	"ratelimiter/ratelimiter"
+	"time"
+)
+
+func main() {
+	taskChan := make(chan func())
+
+	limiter := ratelimiter.New(taskChan, 2, 10, time.Minute)
+	limiter.DoWork()
+
+	go func() {
+		ticker := time.NewTicker(time.Millisecond * 500)
+		counter := 0
+
+		for range ticker.C {
+			if counter == 13 {
+				ticker.Stop()
+				close(taskChan)
+				return
+			}
+
+			task := func() {
+				counter++
+				count := counter
+
+				time.Sleep(time.Second)
+				fmt.Println("Task result", count)
+			}
+
+			taskChan <- task
+		}
+	}()
+
+	limiter.Wait()
+}

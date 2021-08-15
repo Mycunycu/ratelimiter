9c8effa1ec01910641948b469d6924262b83d7d9
Aleksei Deviatovskii <mycunycu2607@gmail.com>
2021-08-15 21:32:06 +0300

    added expected result

--- example/ratelimiter_example.go
+++ example/ratelimiter_example.go
@@ -4,20 +4,22 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Mycunycu/ratelimiter"
+	"github.com/Mycunycu/ratelimiter/ratelimiter"
 )
 
 func Example() {
 	taskChan := make(chan func())
 
-	limiter := ratelimiter.New(taskChan, 2, 10, time.Minute)
+	limiter := ratelimiter.New(taskChan, 2, 5, time.Minute)
 	limiter.DoWork()
 
+	// Task provider simulating
 	go func() {
 		ticker := time.NewTicker(time.Millisecond * 500)
 		counter := 0
 
 		for range ticker.C {
+			// as example closing the task channel
 			if counter == 13 {
 				ticker.Stop()
 				close(taskChan)
@@ -38,3 +40,24 @@ func Example() {
 
 	limiter.Wait()
 }
+
+// Task result 1
+// Task result 2
+// Task result 4
+// Task result 3
+// Task result 5
+
+//               <- waiting for update limits in the time window
+
+// Task result 7
+// Task result 6
+// Task result 9
+// Task result 8
+// Task result 10
+
+//               <- waiting for update limits in the time window
+
+// Task result 12
+// Task result 11
+// Task result 13
+// Task channel are closed
